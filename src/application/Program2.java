1d5c7f732481c7745e8328becca8aea4b5534277
rodrigoaalmeida <rodrigo_a.almeida@outlook.com>
2018-09-12 17:39:32 -0400

    Finalizado

---
+++ src/application/Program2.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		//caminho do arquivo
+		String path = "...\\AulaArquivos\\fala2.txt";
+		BufferedReader br = null;
+		FileReader fr = null;
+		
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
+			String line = br.readLine();
+			
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		}catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}finally {
+			try {
+				if (br != null)
+					br.close();
+				if(fr != null)
+					fr.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
